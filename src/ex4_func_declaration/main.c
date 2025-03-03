13de5618d63af7b1ddd1bca5c8dae1f184a42380
gokceuludogan <gokceuludogan@gmail.com>
2025-03-03 14:05:52 +0000

    Move source files

---
+++ src/ex4_func_declaration/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "add.h"
+
+// Function Declaration: prototype
+
+int main(){
+   printf("%d", add(3, 5));
+   return 0;
+}
+
+// // Function definition
+
+// int add(int a, int b){
+//    return a + b;
+// }
